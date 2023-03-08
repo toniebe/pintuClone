8326a48baca1aeeadf1a14c598cbb176fff699bf
toniebe <toniebe48@gmail.com>
2023-03-08 19:31:43 +0700

    fix[style]: configuration some screen and icon

---
+++ app/(tabs)/discover.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const discover = () => {
+  return (
+    <View>
+      <Text>discover</Text>
+    </View>
+  );
+};
+
+export default discover;
+
+const styles = StyleSheet.create({});
