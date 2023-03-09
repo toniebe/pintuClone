7f8c37e9459ed0c8935b32c0141e21f7e683557b
toniebe <toniebe48@gmail.com>
2023-03-09 20:11:45 +0700

    feat[env]: adding endpoint for pintu apps

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = function (api) {
+  const platform = api.caller((caller) => caller?.platform);
+  const plugins =
+    platform === "web"
+      ? [require.resolve("expo-router/babel")]
+      : ["transform-inline-environment-variables"];
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      require.resolve("expo-router/babel"),
-    ],
+    plugins: plugins,
   };
 };
